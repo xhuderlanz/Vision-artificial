c9afd25e8bc53465faa3ab8b9fb746cd8b90c109
xhuderlanz <sutherlanddwight@hotmail.com>
2022-07-14 21:39:48 -0400

    imagenes

--- Pruebas/prueba imagen.py
+++ Pruebas/prueba imagen.py
@@ -3,8 +3,10 @@
 import numpy as np
 import cv2  
 import matplotlib.pyplot as plt
+import os
 
-def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+
+def rz(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
 
@@ -19,22 +21,72 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
 
     return cv2.resize(image, dim, interpolation=inter)
 
+#detect images 
+path = 'D:\\DocumentosD\\Python\\Vision-artificial\\Imagenes\\TrainF'
+images, gray, B_images, G_images, R_images= [], [], [], [], [] 
+classNames = [] 
+myList = os.listdir(path) 
+
+print('Total classes detected: ',len(myList)) #Imprime la cantidad de archivos que se encuentran en la ruta
+
+for cl in myList: 
+    imgCur = rz(cv2.imread(f'{path}\\{cl}'), height=800) 
+    b, g, r = cv2.split(imgCur)
+    images.append(imgCur) 
+    gray.append(cv2.cvtColor(imgCur, cv2.COLOR_BGR2GRAY))
+    B_images.append(b)
+    G_images.append(g)
+    R_images.append(r)
+    classNames.append(os.path.splitext(cl)[0]) 
+print(classNames)
 
 
 
-path = 'D:\\DocumentosD\\UTP\\brazo\\prueba.jpg'
-img = cv2.imread(path,)
 
-resize = ResizeWithAspectRatio(img, width=960)
+# sift = cv2.SIFT_create(nfeatures=500)
 
-cv2.imshow('resize', resize)
-cv2.waitKey(0)
+# kp, des, kp_images  = [], [], []
+
+# for i in range(len(images)):
+#     k, d = sift.detectAndCompute(images[i], None)
+#     kp_images.append(cv2.drawKeypoints(images[i], k, None))
+#     kp.append(k)
+#     des.append(d)
+
+# himages, hkp_images = [], []
+# for i in range(len(images)):
+#     himages.append(images[i])
+#     hkp_images.append(kp_images[i])
+
+# show = cv2.resize( np.vstack( (rz(np.hstack( himages ), width=1300), rz(np.hstack( hkp_images ), width=1300)) ), (1280,720), interpolation=cv2.INTER_AREA )
+
+# cv2.imshow('Imagen', show)
 
 
-# cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
-# im = cv2.imread(path)                    # Read image
-# imS = cv2.resize(im, (960, 540))                # Resize image
-# cv2.imshow("output", imS)                       # Show image
-# cv2.waitKey(0)   
+fig1 = plt.figure('Histogramas')
+fig1.subplots_adjust(hspace=0.5, wspace=0.5)
+
+for i in range(len(gray)):
+    
+    hist = cv2.calcHist([gray[i]], [0], None, [256], [0,256])
+    ax = fig1.add_subplot(1, 1, i+1)
+    ax.plot(hist)
+    ax.set_xlabel("Bins")
+    ax.set_ylabel("# de pixeles")
+    ax.set_title("Histograma imagen " + str(i))
+    ax.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
+    # Pintar los ejes pasando por (0,0)
+    ax.axhline(0, color='black', linewidth=0.5)
+    
+plt.show()
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+# # cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
+# # im = cv2.imread(path)                    # Read image
+# # imS = cv2.resize(im, (960, 540))                # Resize image
+# # cv2.imshow("output", imS)                       # Show image
+# # cv2.waitKey(0)   
 
 
---
+++ Pruebas/prueba_imagen2.py
@@ -0,0 +1,104 @@
+#D:\DocumentosD\UTP\brazo\prueba.jpg
+
+from ctypes import sizeof
+import numpy as np
+import cv2  
+import matplotlib.pyplot as plt
+import os
+
+
+def rz(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+#detect images 
+path = 'D:\\DocumentosD\\Python\\Vision-artificial\\Imagenes\\TrainF'
+images = []
+
+colors = np.array([[],[],[],[]]) #[gray, b, g, r]
+classNames = [] 
+myList = os.listdir(path) 
+
+print('Total classes detected: ',len(myList)) #Imprime la cantidad de archivos que se encuentran en la ruta
+
+for cl in myList: 
+    imgCur = rz(cv2.imread(f'{path}\\{cl}'), height=800) 
+    b, g, r = cv2.split(imgCur)
+    images.append(imgCur) 
+    colors[0].append(cv2.cvtColor(imgCur, cv2.COLOR_BGR2GRAY))
+    colors[1].append(b)
+    colors[2].append(g)
+    colors[3].append(r)
+    classNames.append(os.path.splitext(cl)[0]) 
+print(classNames)
+
+print('dimension: ', colors.shape())
+# #colores
+# h1, h2 = [], []
+# for i in range():
+    
+
+# show = cv2.resize( np.vstack( (rz(np.hstack( h1 ), width=1300), rz(np.hstack( h2 ), width=1300)) ), (1280,720), interpolation=cv2.INTER_AREA )
+
+# cv2.imshow('Imagen', show)
+
+
+
+# sift = cv2.SIFT_create(nfeatures=500)
+
+# kp, des, kp_images  = [], [], []
+
+# for i in range(len(images)):
+#     k, d = sift.detectAndCompute(images[i], None)
+#     kp_images.append(cv2.drawKeypoints(images[i], k, None))
+#     kp.append(k)
+#     des.append(d)
+
+# himages, hkp_images = [], []
+# for i in range(len(images)):
+#     himages.append(images[i])
+#     hkp_images.append(kp_images[i])
+
+# show = cv2.resize( np.vstack( (rz(np.hstack( himages ), width=1300), rz(np.hstack( hkp_images ), width=1300)) ), (1280,720), interpolation=cv2.INTER_AREA )
+
+# cv2.imshow('Imagen', show)
+
+
+# fig1 = plt.figure('Histogramas')
+# fig1.subplots_adjust(hspace=0.5, wspace=0.5)
+
+# for i in range(len(gray)):
+    
+#     hist = cv2.calcHist([gray[i]], [0], None, [256], [0,256])
+#     ax = fig1.add_subplot(1, 1, i+1)
+#     ax.plot(hist)
+#     ax.set_xlabel("Bins")
+#     ax.set_ylabel("# de pixeles")
+#     ax.set_title("Histograma imagen " + str(i))
+#     ax.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
+#     # Pintar los ejes pasando por (0,0)
+#     ax.axhline(0, color='black', linewidth=0.5)
+    
+# plt.show()
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+# # cv2.namedWindow("output", cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
+# # im = cv2.imread(path)                    # Read image
+# # imS = cv2.resize(im, (960, 540))                # Resize image
+# # cv2.imshow("output", imS)                       # Show image
+# # cv2.waitKey(0)   
+
+
